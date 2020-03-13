3109e31b9836df30520c35aec7046cf4ff548dff
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2020-03-13 21:35:31 +0900

    cache

---
+++ nomcsv2json.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const _ = require('lodash');
+const XLSX = require('xlsx');
+const moment = require('moment');
+const path = require('path');
+const fs = require('fs');
+
+let arrTrainX = [];
+let arrTrainT = [];
+
+let arrDate = [];
+let arrUPRO = [];
+let arrFXY = [];
+let arrT1570 = [];
+
+const DESIRED_ERROR = 0.005;
+const PERIOD = 53;
+let days; //学習データ数
+
+const CSV_PATH = path.join('T:\\ProgramFilesT\\pleiades\\workspace\\node225', 'nt1570.csv');
+const workbook = XLSX.readFile(CSV_PATH);
+const worksheet = workbook.Sheets['Sheet1'];
+const arrHashExcel = XLSX.utils.sheet_to_json(worksheet);
+
+_.forEach(arrHashExcel, HashExcel => {
+    const nExcelValue = HashExcel.date - 2; //Excelでは1900年が閏年判定されるので2月29日まである。-2
+    const mDate = moment(['1900', '0', '1']).add(nExcelValue, 'days').format('YYYY-MM-DD');
+
+    arrDate.push(mDate);
+    arrUPRO.push(HashExcel.upro);
+    arrFXY.push(HashExcel.fxy);
+    arrT1570.push(HashExcel.t1570);
+});
+
+const ARR_LEN = arrDate.length;
+arrDate = _.drop(arrDate); //前日比の変化率なので初日を除外
+
+let arrChangeUPRO = [];
+let arrChangeFXY = [];
+let arrChangeT1570 = [];
+
+for (let i = 0; i < ARR_LEN; i++) {
+    if (0 < i) {
+        const _f0 = (arrUPRO[i] / arrUPRO[i - 1]) * 100;
+        arrChangeUPRO.push(_f0);
+
+        const _f1 = (arrFXY[i] / arrFXY[i - 1]) * 100;
+        arrChangeFXY.push(_f1);
+
+        const _f2 = (arrT1570[i] / arrT1570[i - 1]) * 100;
+        arrChangeT1570.push(_f2);
+    }
+}
+
+days = ARR_LEN - 1;
+
+const SKIP = days - PERIOD; //スキップする日数
+days = PERIOD;
+
+arrDate = _.drop(arrDate, SKIP);
+arrChangeUPRO = _.drop(arrChangeUPRO, SKIP);
+arrChangeFXY = _.drop(arrChangeFXY, SKIP);
+arrChangeT1570 = _.drop(arrChangeT1570, SKIP);
+
+const UPRO_div = _.max(arrChangeUPRO) * (1 + DESIRED_ERROR); //スキップ後に最大値取得
+const FXY_div = _.max(arrChangeFXY) * (1 + DESIRED_ERROR); //スキップ後に最大値取得
+const T1570_div = _.max(arrChangeT1570) * (1 + DESIRED_ERROR); //スキップ後の最大値に期待値誤差を加えて除数とする
+//学習データ正規化
+for (let i = 0; i < days; i++) {
+    const _x0 = arrChangeUPRO[i] / UPRO_div;
+    const _x1 = arrChangeFXY[i] / FXY_div;
+
+    arrTrainX.push([_x0, _x1]); //training data without bias
+    arrTrainT.push([arrChangeT1570[i] / T1570_div]); //teacher data
+}
+
+const arrTrainData = _.zipWith(arrTrainX, arrTrainT, arrDate, (x, t, d) => {
+    return {
+        input: x,
+        output: t,
+        date: d
+    }
+});
+
+fs.writeFileSync('./json/py225.json', JSON.stringify(arrTrainData), 'utf8');
---
+++ paindevice.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import math
+import time
+import random
+import matplotlib.pyplot as plt
+
+DESIRED_ERROR = 0.005
+OUT_NODE = 1
+ETA = 0.5
+THRESHOLD = 1000000
+
+
+def sigmoid(a: float) -> float:
+    if a < 0:
+        return 1 - 1 / (1 + math.exp(a))
+    else:
+        return 1 / (1 + math.exp(-a))
+
+
+dsigmoid = lambda a: a * (1 - a)
+dmax = lambda a: a if (0 < a) == 1 else 0
+
+IN_NODE = HID_NODE = None
+hid = out = None
+delta_hid = delta_out = None
+
+epoch = days = 0
+fError = 0.05
+
+x = t = None
+
+v = []
+w = []
+
+isPlot = True
+
+
+def findHidOut(n: int):
+    for i in range(HID_NODE):
+        dot_h = 0
+        for j in range(IN_NODE):
+            dot_h += x[n][j] * v[i][j]
+        hid[i] = sigmoid(dot_h)
+
+    hid[HID_NODE - 1] = random.random()
+
+    for i in range(OUT_NODE):
+        dot_o = 0
+        for j in range(HID_NODE):
+            dot_o += w[i][j] * hid[j]
+        out[i] = sigmoid(dot_o)
+
+
+def printResult():
+    arrNet = []
+    s = 0
+    for i in range(days):
+        findHidOut(i)
+        rd_teacher = round(t[i][0], 3)
+        rd_out = round(out[0], 3)
+        net = 100 * (t[i][0] - out[0]) / t[i][0]
+        s += net
+        arrNet.append(net)
+        str_date = arrHsh[i]["date"]
+        print(f"{str_date} teacher: {rd_teacher} out: {rd_out} valance: {round(s, 3)}")
+
+    rd_err = round(fError, 5)
+    print(f"epoch: {epoch} final err: {rd_err} days: {days}")
+    print(f"time: {round((time_ed - time_st), 2)} sec.")
+
+
+def addBias(hsh: dict) -> dict:
+    arrInput = hsh["input"]
+    # arrInput.append(random.random() * -1)  # add bias
+    arrInput.append(-1)  # add bias
+    return arrInput
+
+
+if __name__ == "__main__":
+    f = open("./json/py225.json", "r")  # xor | cell30 | py225
+    arrHsh = json.load(f)
+
+    x = list(map(addBias, arrHsh))
+    t = list(map(lambda hsh: hsh["output"], arrHsh))
+
+    IN_NODE = len(x[0])  # get input length include bias
+    HID_NODE = IN_NODE + 1
+    days = len(x)
+
+    hid = [0] * HID_NODE
+    out = [0] * OUT_NODE
+    delta_hid = [0] * HID_NODE
+    delta_out = [0] * OUT_NODE
+
+    arrErr = []
+
+    for i in range(HID_NODE):
+        v.append([])
+    for i in range(OUT_NODE):
+        w.append([])
+
+    for i in range(HID_NODE):
+        for j in range(IN_NODE):
+            v[i].append(random.random())
+    for i in range(OUT_NODE):
+        for j in range(HID_NODE):
+            w[i].append(random.random())
+
+    time_st = time.time()
+
+    while DESIRED_ERROR < fError:
+        epoch += 1
+        fError = 0
+
+        for n in range(days):
+            findHidOut(n)
+
+            for k in range(OUT_NODE):
+                fError += 0.5 * (t[n][k] - out[k]) ** 2
+                delta_out[k] = (t[n][k] - out[k]) * out[k] * (1 - out[k])
+
+            for k in range(OUT_NODE):
+                for j in range(HID_NODE):
+                    w[k][j] += ETA * delta_out[k] * hid[j]
+
+            for i in range(HID_NODE):
+                delta_hid[i] = 0
+
+                for k in range(OUT_NODE):
+                    delta_hid[i] += delta_out[k] * w[k][i]
+
+                delta_hid[i] = dsigmoid(hid[i]) * delta_hid[i]
+
+            for i in range(HID_NODE):
+                for j in range(IN_NODE):
+                    v[i][j] += ETA * delta_hid[i] * x[n][j]
+        # for days
+        if isPlot:
+            if epoch % 100 == 0:
+                # print(f"{epoch}: {fError}")
+                arrErr.append(fError)
+                pass
+
+        if THRESHOLD < epoch:
+            print("force quit")
+            break
+    # while
+    time_ed = time.time()
+    printResult()
+    # show plot
+    if isPlot:
+        plt.plot(arrErr)
+        plt.show()
