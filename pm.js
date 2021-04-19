a8c03f8f9fb8ff845c0e9a7125e2b15e4ba8d8a0
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2021-04-19 13:52:38 +0900

    promise all

--- pm.js
+++ pm.js
@@ -12,7 +12,7 @@ let HID_NODE; //隠れノード数
 const OUT_NODE = 1; //出力ノード数
 
 const ETA = 0.5; //学習係数
-const THRESH = 50000;
+const THRESH = 500000;
 
 const sigmoid = x => 1 / (1 + Math.exp(-x)); //シグモイド関数
 const dsigmoid = x => x * (1 - x); //シグモイド関数微分
@@ -97,6 +97,45 @@ const printResult = (arrHsh, DIV_T) => {
     console.log(`FinalErr: ${fError.toFixed(5)}\n`);
 }
 
+const psm = (arrHsh, DIV_T) => {
+    while (epoch < THRESH) {
+        epoch++;
+        fError = 0;
+
+        for (let n = 0; n < DATA_LEN; n++) {
+            updateHidOut(n);
+
+            for (let k = 0; k < OUT_NODE; k++) {
+                fError += 0.5 * Math.pow((t[n][k] - out[k]), 2); //誤差を日数分加算する
+                // Δw
+                delta_out[k] = (t[n][k] - out[k]) * out[k] * (1 - out[k]); //δ=(t-o)*f'(net); net=Σwo; δo/δnet=f'(net);
+            }
+
+            for (let k = 0; k < OUT_NODE; k++) { // Δw
+                for (let j = 0; j < HID_NODE; j++) {
+                    w[k][j] += ETA * delta_out[k] * hid[j]; //Δw=ηδH
+                }
+            }
+
+            for (let i = 0; i < HID_NODE; i++) { // Δv
+                delta_hid[i] = 0;
+
+                for (let k = 0; k < OUT_NODE; k++) {
+                    delta_hid[i] += delta_out[k] * w[k][i]; //Σδw
+                }
+
+                delta_hid[i] = dsigmoid(hid[i]) * delta_hid[i]; //H(1-H)*Σδw
+            }
+
+            for (let i = 0; i < HID_NODE; i++) { // Δv
+                for (let j = 0; j < IN_NODE; j++) {
+                    v[i][j] += ETA * delta_hid[i] * x[n][j]; //Δu=ηH(1-H)XΣδw
+                }
+            }
+        }
+    } //while
+    printResult(arrHsh, DIV_T);
+}
 
 //main
 (async () => {
@@ -107,7 +146,7 @@ const printResult = (arrHsh, DIV_T) => {
 
     const arrStrFile = fs.readdirSync(BATCH_PATH);
 
-    await Promise.all(_.map(arrStrFile, strFile => {
+    await Promise.all(_.map(arrStrFile, (strFile) => {
         //グローバル変数初期化
         hid = [];
         out = [];
@@ -155,44 +194,13 @@ const printResult = (arrHsh, DIV_T) => {
         }
 
         return new Promise((resolve, reject) => {
-            while (epoch < THRESH) {
-                epoch++;
-                fError = 0;
-
-                for (let n = 0; n < DATA_LEN; n++) {
-                    updateHidOut(n);
-
-                    for (let k = 0; k < OUT_NODE; k++) {
-                        fError += 0.5 * Math.pow((t[n][k] - out[k]), 2); //誤差を日数分加算する
-                        // Δw
-                        delta_out[k] = (t[n][k] - out[k]) * out[k] * (1 - out[k]); //δ=(t-o)*f'(net); net=Σwo; δo/δnet=f'(net);
-                    }
-
-                    for (let k = 0; k < OUT_NODE; k++) { // Δw
-                        for (let j = 0; j < HID_NODE; j++) {
-                            w[k][j] += ETA * delta_out[k] * hid[j]; //Δw=ηδH
-                        }
-                    }
-
-                    for (let i = 0; i < HID_NODE; i++) { // Δv
-                        delta_hid[i] = 0;
-
-                        for (let k = 0; k < OUT_NODE; k++) {
-                            delta_hid[i] += delta_out[k] * w[k][i]; //Σδw
-                        }
-
-                        delta_hid[i] = dsigmoid(hid[i]) * delta_hid[i]; //H(1-H)*Σδw
-                    }
-
-                    for (let i = 0; i < HID_NODE; i++) { // Δv
-                        for (let j = 0; j < IN_NODE; j++) {
-                            v[i][j] += ETA * delta_hid[i] * x[n][j]; //Δu=ηH(1-H)XΣδw
-                        }
-                    }
-                }
-            } //while
-            printResult(arrHsh, DIV_T);
-            resolve();
+            //let n = Math.floor(Math.random() * 1);
+            //n = 3;
+            //n *= 1000
+            setTimeout(() => {
+                psm(arrHsh, DIV_T);
+                resolve();
+            }, 0);
         });
     }));
     //計測終了
