f824dd03606ee045d829c102a49fa93fa2c6fe53
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2024-03-08 12:41:25 +0900

    daily

--- run_Windows.bat
+++ run_Windows.bat
@@ -1,5 +1,5 @@
 @echo off
 @rem powershell -Command "Start-Process -NoNewWindow 'npm' -ArgumentList 'run main+batch'"
 @rem powershell -Command "Start-Process -NoNewWindow 'npm' -ArgumentList 'run sakata+batch'"
-start pwsh -NoExit -Command "node ./nodejs/main-batch.js > ./result/main-batch.log; python ./util/plot-multi.py"
-start pwsh -NoExit -Command "node ./nodejs/braindevice-batch.js > ./result/sakata-batch.log"
+start pwsh -NoExit -Command "node ./nodejs/main-batch.js > ./result/main-batch.log"
+start pwsh -NoExit -Command "node ./nodejs/braindevice-batch.js > ./result/sakata-batch.log; python ./util/plot-multi.py"
