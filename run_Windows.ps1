d5c45d9fd48dd190c2dbefdd4eb78976caf41e90
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2024-06-13 17:42:47 +0900

    pwsh

--- run_Windows.ps1
+++ run_Windows.ps1
@@ -1,8 +1,33 @@
-# result/output.log を事前にクリア（存在しない場合は作成）
-$null > ./result/output.log
+# ログファイルのパスを変数として定義
+$logFile = "./result/output3.log"
 
-# batch フォルダ内の全ての .json ファイルに対してループ
+# バッチファイル生成
+python ./utils/slicebatch.py
+
+# 開始時間の記録
+$startTime = Get-Date
+
+# 出力ファイルを事前にクリアして現在の日付と時刻を追記
+Get-Date | Out-File -FilePath $logFile -Encoding utf8
+
+# batchフォルダ内の全ての.jsonファイルに対してループ
 Get-ChildItem -Path ./batch -Filter *.json | ForEach-Object {
     # ファイルの内容を inlinestd.py に渡し、結果を出力ファイルに追記
-    Get-Content $_.FullName | python ./src/python/inlinestd.py >> ./result/output.log
+    Get-Content $_.FullName | python ./src/python/inlinestd.py | Out-File -FilePath $logFile -Append -Encoding utf8
 }
+
+# 画像出力
+python ./src/python/plot-batch.py $logFile
+
+# 終了時間の記録
+$endTime = Get-Date
+
+# 経過時間の計算（秒単位）
+$elapsedTime = $endTime - $startTime
+
+# 経過時間を分と秒に変換
+$minutes = [math]::Floor($elapsedTime.TotalMinutes)
+$seconds = $elapsedTime.Seconds
+
+# 経過時間をログファイルに追記
+"Time: $minutes min $seconds sec." | Out-File -FilePath $logFile -Append -Encoding utf8
